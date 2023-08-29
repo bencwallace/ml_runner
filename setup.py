1998b62edb4e5face8967b8197df748a3edbaad6
Benjamin Wallace <bwallace@irobot.com>
2023-08-29 21:03:25 +0200

    use cpu torch

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-# hacky solution for editable installs while awaiting next poetry release
-setup(
-    name="ml_training",
-    author="Ben Wallace",
-    author_email="bencwallace@gmail.com",
-    packages=find_packages("ml_training"),
-)
