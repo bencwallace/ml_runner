d4fbebcc36357357a7974cb8e3b3e5b17daeb13d
Ben Wallace <bencwallace@gmail.com>
2023-08-30 08:10:36 -0400

    format with black

--- ml_runner/entrypoint/fit.py
+++ ml_runner/entrypoint/fit.py
@@ -2,5 +2,10 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
 
-def fit(pl_module: pl.LightningModule, trainer: pl.Trainer, train_loader: DataLoader, val_loader: DataLoader):
+def fit(
+    pl_module: pl.LightningModule,
+    trainer: pl.Trainer,
+    train_loader: DataLoader,
+    val_loader: DataLoader,
+):
     trainer.fit(pl_module, train_loader, val_loader)
