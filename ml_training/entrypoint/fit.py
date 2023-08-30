9d47c09e539e2e5354a95451ba053eeb2cc5c950
Benjamin Wallace <bwallace@irobot.com>
2023-08-30 04:11:42 -0400

    add validation set

--- ml_training/entrypoint/fit.py
+++ ml_training/entrypoint/fit.py
@@ -2,5 +2,5 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
 
-def fit(pl_module: pl.LightningModule, trainer: pl.Trainer, train_loader: DataLoader):
-    trainer.fit(pl_module, train_loader)
+def fit(pl_module: pl.LightningModule, trainer: pl.Trainer, train_loader: DataLoader, val_loader: DataLoader):
+    trainer.fit(pl_module, train_loader, val_loader)
--- ml_training/module/generic.py
+++ ml_training/module/generic.py
@@ -17,6 +17,13 @@ class GenericModule(pl.LightningModule):
         self.log("train/loss", loss, prog_bar=True, on_step=False, on_epoch=True)
         return loss
 
+    def validation_step(self, batch, _) -> STEP_OUTPUT | None:
+        x, y = batch
+        logits = self.model(x)
+        loss = self.loss_fn(logits, y)
+        self.log("val/loss", loss, prog_bar=True, on_step=False, on_epoch=True)
+        return loss
+
     def configure_optimizers(self) -> Any:
         optimizer = self.optimizer(params=self.parameters())
         return {
